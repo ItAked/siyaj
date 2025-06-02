a63f8178be05bd8a2e2da16e93d2978aee484ee8
xilujain <lujainboo@gmail.com>
2025-06-02 13:01:13 +0300

    Update font and logo dimensions in layout and sidebar components
    - Changed font from Outfit to Tajawal in layout.tsx
    - Updated logo dimensions from 150x40 to 220x220 in AppSidebar.tsx
    - Adjusted logo source for dark mode to use the same logo in AppSidebar.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import { Outfit } from 'next/font/google';
+import { Tajawal } from 'next/font/google';
 import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 
-const outfit = Outfit({
-  subsets: ["latin"],
+const tajawal = Tajawal({
+  subsets: ["Tajawal"]
 });
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
+      <body className={`${tajawal.className} dark:bg-gray-900`}>
         <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
