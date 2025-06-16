6a5422a6fe3a34226659a93f26ef8fd97baf23f5
xilujain <lujainboo@gmail.com>
2025-06-16 10:13:35 +0300

    feat: implement lawyer data fetching and update lawyers table component

--- src/app/admin/(others-pages)/(tables)/lawyers/page.tsx
+++ src/app/admin/(others-pages)/(tables)/lawyers/page.tsx
@@ -4,107 +4,22 @@ import Select from "@/components/form/Select";
 import { Modal } from "@/components/ui/modal";
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
 import { useModal } from "@/hooks/useModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { get } from "../../../../../../server/lawyers";
 
-interface Order {
+interface Lawyers {
   id: number;
-  user: {
-    image: string;
-    name: string;
-    role: string;
-  };
-  projectName: string;
-  team: {
-    images: string[];
-  };
-  budget: string;
+  email: string;
+  employer: string
+  name: string
+  phone: string
+  created_at: string
 }
 
-// Define the table data using the interface
-const tableData: Order[] = [
-  {
-    id: 1,
-    user: {
-      image: "/images/user/user-17.jpg",
-      name: "Lindsey Curtis",
-      role: "Web Designer",
-    },
-    projectName: "Agency Website",
-    team: {
-      images: [
-        "/images/user/user-22.jpg",
-        "/images/user/user-23.jpg",
-        "/images/user/user-24.jpg",
-      ],
-    },
-    budget: "3.9K"
-  },
-  {
-    id: 2,
-    user: {
-      image: "/images/user/user-18.jpg",
-      name: "Kaiya George",
-      role: "Project Manager",
-    },
-    projectName: "Technology",
-    team: {
-      images: ["/images/user/user-25.jpg", "/images/user/user-26.jpg"],
-    },
-    budget: "24.9K"
-  },
-  {
-    id: 3,
-    user: {
-      image: "/images/user/user-17.jpg",
-      name: "Zain Geidt",
-      role: "Content Writing",
-    },
-    projectName: "Blog Writing",
-    team: {
-      images: ["/images/user/user-27.jpg"],
-    },
-    budget: "12.7K"
-  },
-  {
-    id: 4,
-    user: {
-      image: "/images/user/user-20.jpg",
-      name: "Abram Schleifer",
-      role: "Digital Marketer",
-    },
-    projectName: "Social Media",
-    team: {
-      images: [
-        "/images/user/user-28.jpg",
-        "/images/user/user-29.jpg",
-        "/images/user/user-30.jpg",
-      ],
-    },
-    budget: "2.8K"
-  },
-  {
-    id: 5,
-    user: {
-      image: "/images/user/user-21.jpg",
-      name: "Carla George",
-      role: "Front-end Developer",
-    },
-    projectName: "Website",
-    team: {
-      images: [
-        "/images/user/user-31.jpg",
-        "/images/user/user-32.jpg",
-        "/images/user/user-33.jpg",
-      ],
-    },
-    budget: "4.5K"
-  },
-];
-
 const options = [
-    { value: "marketing", label: "Marketing" },
-    { value: "template", label: "Template" },
-    { value: "development", label: "Development" },
+    { value: "القضية الأولى", label: "القضية الأولى" },
+    { value: "القضية الثانية", label: "القضية الثانية" },
+    { value: "القضية الثالثة", label: "القضية الثالثة" },
   ];
 
   const status = [
@@ -119,11 +34,19 @@ export default function Lawyers() {
     const [lawyerName, setLawyerName] = useState("")
     const [lawyerEmail, setLawyerEmail] = useState("")
     const [lawyerPassword, setLawyerPassword] = useState("")
+    const [lawyers, setLawyers] = useState<Lawyers[]>([])
 
     function resetModalFeild(){
       setLawyerName("")
       setLawyerEmail("")
       setLawyerPassword("")
+    }
+
+    async function getLawyers() {
+      const response = await get()
+
+      setLawyers(response.data.data)
+      
     }
 
     function handleAddLawyer() {
@@ -139,6 +62,10 @@ export default function Lawyers() {
     const handleSelectChange = (value: string) => {
     console.log("Selected value:", value);
   };
+
+  useEffect(() => {
+    getLawyers()
+  }, [])
 
     return (
         <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
@@ -263,22 +190,19 @@ export default function Lawyers() {
                 {/* Table Body */}
 
                 <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
-                    {tableData.map((product) => (
-                    <TableRow key={product.id} className="">
+                    {lawyers.map((lawyer) => (
+                    <TableRow key={lawyer.id} className="">
                         <TableCell className="py-3">
                         <div className="flex items-center gap-3">
                             <div>
-                            <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                                {product.projectName}
-                            </p>
-                            <span className="text-gray-500 text-theme-xs dark:text-gray-400">
-                                {product.budget}
-                            </span>
+                              <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                  {lawyer.name}
+                              </p>
                             </div>
                         </div>
                         </TableCell>
                         <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                        {product.budget}
+                        {lawyer.created_at}
                         </TableCell>
                         <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
                         <Select className="max-w-xs" options={options} onChange={handleSelectChange} />
