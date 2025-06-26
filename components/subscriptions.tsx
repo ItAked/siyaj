7629db108b2433b7f8d239ceab76fe14e457e7bf
xilujain <lujainboo@gmail.com>
2025-06-26 15:58:39 +0300

    Fix: adjust heading style for smaller screens in Subscriptions component

--- components/subscriptions.tsx
+++ components/subscriptions.tsx
@@ -19,7 +19,7 @@ const Subscriptions = () => {
 
   return (
     <div className="w-full max-w-6xl mx-auto px-4 py-8 my-32" id='packages'>
-      <h2 className="text-6xl font-light text-center mb-2">اختر باقتك</h2>
+      <h2 className="text-6xl font-light text-center mb-2 max-sm:text-4xl">اختر باقتك</h2>
       
       {/* Subscription cards */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 my-14">
