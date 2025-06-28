6aee4f0b02cdbcfb0c3157560cf1d00e3ab01513
xilujain <lujainboo@gmail.com>
2025-06-28 10:15:15 +0300

    refactor: clean up commented-out code in post function in assign_cases.ts

--- server/CasesServer/assign_cases.ts
+++ server/CasesServer/assign_cases.ts
@@ -1,11 +1,5 @@
 import axios from "axios"
 
-// export async function post (id: number, cases: number[]) {
-//     const response = await axios.post(`http://127.0.0.1:8000/api/assign-case/${id}`,cases ,{headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}})
-
-//     return response
-// }
-
 export async function post(id: number, data: { cases: number[], is_checked: boolean }) {
     const response = await axios.post(
         `http://127.0.0.1:8000/api/assign-case/${id}`,
