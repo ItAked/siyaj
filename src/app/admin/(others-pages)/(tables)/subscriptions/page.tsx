7747e093157a50b2c28670e16f61956cccaca43d
xilujain <lujainboo@gmail.com>
2025-06-29 10:49:21 +0300

    feat: add CreateFeature function and integrate feature addition in Subscriptions component

--- src/app/admin/(others-pages)/(tables)/subscriptions/page.tsx
+++ src/app/admin/(others-pages)/(tables)/subscriptions/page.tsx
@@ -3,6 +3,8 @@
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
 import { useEffect, useState } from "react";
 import { readSubscriptions } from "../../../../../../server/read_subscriptions";
+import { CreateFeature } from "../../../../../../server/create_new_feature";
+import { useModal } from "@/hooks/useModal";
 
 interface Faetures {
     id: number;
@@ -19,7 +21,8 @@ interface Subscription {
 }
 
 export default function Subscriptions() {
-    const [lawyerName, setLawyerName] = useState("");
+    const { closeModal } = useModal();
+    const [titleFeature, setTitleFeature] = useState("");
     const [lawyerEmail, setLawyerEmail] = useState("");
     const [lawyerPassword, setLawyerPassword] = useState("");
     const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
@@ -35,6 +38,22 @@ export default function Subscriptions() {
             console.error("Failed to fetch lawyers:", error);
         } finally {
             setIsLoading(false);
+        }
+    }
+
+    async function handleAddFeature() {
+        setIsLoading(true)
+        try {
+            const form = new FormData()
+
+            form.append('title', titleFeature)
+            await CreateFeature(form)
+            closeModal();
+            getSubscriptions()
+        } catch (error) {
+            console.error("Failed to fetch lawyers:", error);
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -59,7 +78,7 @@ export default function Subscriptions() {
                                 <div>
                                     <div>
                                         <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">الباقة</label>
-                                        <input name="laywer_name" type="text" value={lawyerName} onChange={(e) => setLawyerName(e.target.value)}
+                                        <input name="laywer_name" type="text" value={titleFeature} onChange={(e) => setTitleFeature(e.target.value)}
                                         className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800
                                         shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10
                                         dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
@@ -98,12 +117,47 @@ export default function Subscriptions() {
                         </div>
                     </div>
                 </dialog>
+                <dialog id="my_modal_6" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <div className="flex flex-col px-2 overflow-y-auto custom-scrollbar">
+                            <div>
+                                <h5 className="mb-2 font-semibold text-gray-800 modal-title text-center text-theme-xl dark:text-white/90 lg:text-2xl">تعديل المميزات</h5>
+                            </div>
+                            <div className="mt-8">
+                                <div>
+                                    <div>
+                                        <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">اضافة ميزة جديدة</label>
+                                        <input name="laywer_name" type="text" value={titleFeature} onChange={(e) => setTitleFeature(e.target.value)}
+                                        className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800
+                                        shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10
+                                        dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex items-center gap-3 mt-6 modal-footer sm:justify-end modal-action">
+                                <button onClick={handleAddFeature} type="button" className="btn btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4
+                                py-2.5 text-sm font-medium text-white hover:bg-yellow-600 sm:w-auto">+</button>
+                                <form method="dialog">
+                                    <button className="btn">إغلاق</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </dialog>
 
-                <button onClick={() => {
-                        const modal = document.getElementById('my_modal_5') as HTMLDialogElement | null;
-                        if (modal) modal.showModal();
-                    }} type="button" className="btn btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4
-                                py-2.5 text-sm font-medium text-white hover:bg-yellow-600 sm:w-auto">إضافة محامي</button>
+                <div className="flex items-center">
+                    <button onClick={() => {
+                            const modal = document.getElementById('my_modal_5') as HTMLDialogElement | null;
+                            if (modal) modal.showModal();
+                        }} type="button" className="btn btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4
+                                    py-2.5 text-sm font-medium text-white hover:bg-yellow-600 sm:w-auto">إضافة محامي</button>
+                    <button onClick={() => {
+                            const modal = document.getElementById('my_modal_6') as HTMLDialogElement | null;
+                            if (modal) modal.showModal();
+                        }} type="button" className="btn btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4
+                                    py-2.5 text-sm font-medium text-white hover:bg-yellow-600 sm:w-auto">تعديل المميزات</button>
+                </div>
+
             </div>
 
             <div className="max-w-full overflow-x-auto">
