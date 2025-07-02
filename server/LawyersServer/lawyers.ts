a757a88fe75cb5fdec7d0a460137a810a4d21309
xilujain <lujainboo@gmail.com>
2025-07-02 11:59:19 +0300

    feat: enhance practitioners retrieval with search and pagination, update UI components

--- server/LawyersServer/lawyers.ts
+++ server/LawyersServer/lawyers.ts
@@ -5,8 +5,6 @@ export async function get(search?: string, page: number = 1) {
 
     if (search) params.search = search;
     
-    
-    
     const response = await axios.get('http://127.0.0.1:8000/api/lawyers', { params, headers:{Authorization:`Bearer ${localStorage.getItem('token')}`} });
 
     return response;
