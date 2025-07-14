6d45d3bd6f7ee9aef68840567c355bb307eadf75
xilujain <lujainboo@gmail.com>
2025-07-14 09:05:51 +0300

    remove all console.log

--- server/PaymentServer/index.ts
+++ server/PaymentServer/index.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export async function getPayments(page: number = 1) {
     const params: Record<string, unknown> = {page};
-    console.log(params);
     
     const response = await axios.get('https://api.moyasar.com/v1/payments', {params, headers: {Authorization: 'Basic c2tfdGVzdF9UUFNGUmVRVnl5a0xVMkd6VXc3djhhOERKMktuWXZVVEZWNHNhZmduOiIi'}})
     
