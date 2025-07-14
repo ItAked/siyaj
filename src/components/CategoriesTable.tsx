0fdc4d961ba0aeb1440bdffa5694f60158469c04
xilujain <lujainboo@gmail.com>
2025-07-14 09:05:25 +0300

    get all payments

--- src/components/CategoriesTable.tsx
+++ src/components/CategoriesTable.tsx
@@ -1,38 +1,35 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { HorizontaLDots } from "../icons";
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "./ui/table";
 import Pagination from "./tables/Pagination";
-import { readUserCategories } from "../../server/SubscriptionsServer/read_user_categories";
 import Badge from "./ui/badge/Badge";
-
-interface UserCategory {
-    id: number;
-    name: string;
-    created_at: string;
-    expired_at: string;
-    status: string;
-}
+import { getPayments } from "../../server/PaymentServer";
 
 type Meta = {
   current_page?: number;
-  last_page?: number;
+  total_pages?: number;
+}
+
+interface Payments {
+    id: string;
+    description: string;
+    status: string;
 }
 
 export default function CategoriesTable() {
-    const [userCategories, setUserCategories] = useState<UserCategory[]>([])
     const [pagination, setPagination] = useState<Meta>({})
+    const [payments, setPayments] = useState<Payments[]>([])
 
-    async function getUserCategories(page: number){
-        const response = await readUserCategories(page)
-        
-        setUserCategories(response.data)
+    async function readInvoice(page: number) {
+        const response = await getPayments(page)
+
+        setPayments(response.payments)
         setPagination(response.meta)
     }
 
     useEffect(() => {
-        getUserCategories(1)
+        readInvoice(1)
     }, [])
 
     return (
@@ -52,14 +49,14 @@ export default function CategoriesTable() {
                     </svg>
                     <input type="search" name="search" className="w-full" placeholder="إبحث بنوع التصنيف" />
                 </label>
-                <details className="dropdown">
+                {/* <details className="dropdown">
                     <summary className="btn bg-transparent border-none hover:shadow-none shadow-none"><HorizontaLDots /></summary>
                     <ul className="menu dropdown-content bg-base-100 rounded-box z-1 p-2 shadow-sm left-6">
                         <li><label className="label"><input type="radio" defaultChecked className="radio" />الكل</label></li>
                         <li><label className="label"><input type="radio" className="radio" />نشط</label></li>
                         <li><label className="label"><input type="radio" className="radio" />منتهي</label></li>
                     </ul>
-                </details>
+                </details> */}
             </div>
         </div>
         <div className="max-w-full overflow-x-auto">
@@ -70,37 +67,29 @@ export default function CategoriesTable() {
                             نوع التصنيف
                         </TableCell>
                         <TableCell isHeader className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
-                            تاريخ البداية
-                        </TableCell>
-                        <TableCell isHeader className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
-                            تاريخ النهاية
-                        </TableCell>
-                        <TableCell isHeader className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
                             الحالة
                         </TableCell>
                     </TableRow>
                 </TableHeader>
                 
                 <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
-                    {userCategories.map((category) => (
+                    {payments.map((category) => (
                         <TableRow key={category.id} className="">
                             <TableCell className="py-3">
                                 <div className="flex categorys-center gap-3">
                                     <div>
-                                        <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">{category.name}</p>
+                                        <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">{category.description}</p>
                                     </div>
                                 </div>
                             </TableCell>
-                            <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">{category.created_at}</TableCell>
-                            <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">{category.expired_at}</TableCell>
                             <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                                <Badge size="sm" color={category.status === "مفعل" ? "success" : "error"}>{category.status}</Badge>
+                                <Badge size="sm" color={category.status === "paid" ? "success" : "error"}>{category.status}</Badge>
                             </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-            <Pagination currentPage={pagination.current_page || 1} totalPages={pagination.last_page || 1} onPageChange={(page: number): void => {getUserCategories(page);}} />
+            <Pagination currentPage={pagination.current_page || 1} totalPages={pagination.total_pages || 1} onPageChange={(page: number): void => {readInvoice(page);}} />
         </div>
     </div>
   );
