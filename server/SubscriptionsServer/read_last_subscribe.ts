fb400329f4f93becd27c03f1d1fd1aec94d4055d
xilujain <lujainboo@gmail.com>
2025-07-14 13:26:46 +0300

    expire for category subscription

---
+++ server/SubscriptionsServer/read_last_subscribe.ts
@@ -0,0 +1,10 @@
+//http://127.0.0.1:8000
+import axios from "axios";
+
+export async function readLastSubscribe(page: number = 1) {
+    const params: Record<string, unknown> = {page};
+
+    const response = await axios.get('http://127.0.0.1:8000/api/subscribe/showSubscribe', { params, headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}});
+
+    return response.data
+}
--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,13 +1,40 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { readLastSubscribe } from "../../server/SubscriptionsServer/read_last_subscribe"
+
+type LastCategory = {
+    name?: string;
+    expire_at?: string;
+}
+
 const CategoryCard = () => {
+    const [lastCategory, setLastCategoy] = useState<LastCategory>({})
+    async function showLastCategory() {
+        const response = await readLastSubscribe()
+        console.log(response.data);
+        
+        
+        setLastCategoy(response.data)   
+    }
+
+    useEffect(() => {
+        showLastCategory()
+    }, [])
+
     return(
         <>
             <div className="card lg:card-side bg-base-100 shadow-sm dark:bg-gray-900/[0.12] dark:border-2">
                 <div className="card-body dark:text-white">
                     <h2 className="card-title text-2xl font-medium">التصنيف الحالي</h2>
-                    <p className="text-2xl font-base">تصنيف الاستشاري</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn bg-sky-950 text-white dark:bg-brand-400 dark:shadow-none dark:border-none">تجديد الاشتراك</button>
-                    </div>
+                    <p className="text-2xl font-base">{lastCategory.name}</p>
+                    <p className="text-gray-500">{`تاريخ بداية الإشتراك: ${lastCategory.expire_at}`}</p>
+                    <p className="text-error">ملاحظة: ينتهي الإشتراك بعد سنة من تاريخ بداية الإشتراك</p>
+                    {lastCategory.expire_at == null && (
+                        <div className="card-actions justify-end">
+                            <button className="btn bg-sky-950 text-white dark:bg-brand-400 dark:shadow-none dark:border-none">تجديد الاشتراك</button>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
