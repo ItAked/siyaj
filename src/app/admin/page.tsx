bc7ef0d016f283c8dfd4e86764939e6dd8203e44
xilujain <lujainboo@gmail.com>
2025-08-03 12:18:59 +0300

    change authToken

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,11 @@
 'use client'
 
-import React, { useEffect } from "react";
+import React from "react";
 import { CasesStatus } from "../../components/CasesStatus";
 import AllCases from "../../components/AllCases";
-import { getToken } from "../../../utils/auth";
-import { useRouter } from "next/navigation";
 
 
 export default function AdminPanel() {
-  const router = useRouter();
-  useEffect(() => {
-    if (!getToken()) {
-      router.push('/signin');
-    }
-  }, []);
-  
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 'use client'
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { getToken } from "../../utils/auth";
 
 export default function Home() {
     const router = useRouter();
     
-    router.push('/signin');
+    useEffect(() => {
+        if (!getToken()) {
+          router.push('/signin');
+        }
+      }, []);
+    
+    router.push('/admin');
 
     return null;
 }
