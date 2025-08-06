0d7e8e016fb27a6d43985f157dd57a4f8b675a2c
xilujain <lujainboo@gmail.com>
2025-08-06 08:32:49 +0300

    ui of sign in

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -9,7 +9,7 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import Alert from "../ui/alert/Alert";
 import { login } from "../../../services/auth";
-
+import Image from "next/image";
 
 export default function SignInForm() {
   const [showPassword, setShowPassword] = useState(false);
@@ -33,93 +33,58 @@ export default function SignInForm() {
   async function handleOnSubmit(event: FormEvent) {
     try {
       event.preventDefault()
-
-    const formData = new FormData()
-    formData.append('email', user.email)
-    formData.append('password', user.password)
-
-    const response = await login(formData)
-
-    if (response.message.role === undefined) {
-      setErrorMsg(response.message.message)
-    }
-    if(response.message.role === 'lawyer') {
-      setErrorMsg('')
-      router.push('/lawyer')
-    } else {
-      setErrorMsg('الحساب لا يخص المحامين')
-      return;
-    }
-
+      const formData = new FormData()
+      formData.append('email', user.email)
+      formData.append('password', user.password)
+      const response = await login(formData)
+      if (response.message.role === undefined) {
+        setErrorMsg(response.message.message)
+      }
+      if(response.message.role === 'lawyer') {
+        setErrorMsg('')
+        router.push('/lawyer')
+      } else {
+        setErrorMsg('الحساب لا يخص المحامين')
+        return;
+      }
     } catch (error) {
       setErrorMsg(error.response.data.message);
     }
   }
 
   return (
-    <div className="flex flex-col flex-1 lg:w-1/2 w-full mx-auto">
-      <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto mt-60">
-        <div>
-          <div className="mb-5 sm:mb-8 grid gap-y-4">
-            { errorMsg != '' && (<Alert variant={"error"} title={"حدث خطأ !"} message={errorMsg} /> )}
-            <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md text-right">
-              تسجيل الدخول
-            </h1>
-            <p className="text-sm text-gray-500 dark:text-gray-400 text-right">
-              أدخل البريد الإلكتروني والرقم السري للدخول إلى حسابك
-            </p>
-          </div>
-          <div>
-            <form onSubmit={handleOnSubmit}>
-              <div className="space-y-6 text-right">
-                <div>
-                  <Label>
-                    البريد الإلكتروني <span className="text-error-500">*</span>{" "}
-                  </Label>
-                  <Input placeholder="info@gmail.com" name="email" defaultValue={user.email} type="email"onChange={handleUserChange} />
-                </div>
-                <div>
-                  <Label>
-                    كلمة المرور <span className="text-error-500">*</span>{" "}
-                  </Label>
-                  <div className="relative">
-                    <Input
-                      type={showPassword ? "text" : "password"}
-                      placeholder="Enter your password"
-                      onChange={handleUserChange}
-                      name="password"
-                      defaultValue={user.password}
-                    />
-                    <span
-                      onClick={() => setShowPassword(!showPassword)}
-                      className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
-                    >
-                      {showPassword ? (
-                        <EyeIcon className="fill-gray-500 dark:fill-gray-400" />
-                      ) : (
-                        <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400" />
-                      )}
-                    </span>
-                  </div>
-                </div>
-                <div className="flex items-center justify-between" dir="rtl">
-                  <Link
-                    href="/reset-password"
-                    className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
-                  >
-                    نسيت كلمة المرور؟
-                  </Link>
-                </div>
-                <div>
-                  <Button className="w-full" size="sm">
-                    تسجيل الدخول
-                  </Button>
+    <>
+      <section className="py-96 px-36 max-sm:py-64">
+        { errorMsg != '' && (<Alert variant={"error"} title={"حدث خطأ !"} message={errorMsg} /> )}
+        <h1 className="font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">تسجيل الدخول</h1>
+        <div className="flex items-center gap-x-80">
+          <form onSubmit={handleOnSubmit} className="w-1/2 max-sm:w-full">
+            <div className="space-y-6 text-right">
+              <div>
+                <Label>البريد الإلكتروني <span className="text-error-500">*</span></Label>
+                <Input placeholder="info@gmail.com" name="email" defaultValue={user.email} type="email"onChange={handleUserChange} />
+              </div>
+              <div>
+                <Label>كلمة المرور <span className="text-error-500">*</span></Label>
+                <div className="relative">
+                  <Input type={showPassword ? "text" : "password"} placeholder="Enter your password" onChange={handleUserChange} name="password"
+                  defaultValue={user.password} />
+                  <span onClick={() => setShowPassword(!showPassword)} className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2">
+                    {showPassword ? ( <EyeIcon className="fill-gray-500 dark:fill-gray-400" /> ) : ( <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400" /> )}
+                  </span>
                 </div>
               </div>
-            </form>
-          </div>
+              <div className="flex items-center justify-between" dir="rtl">
+                <Link href="/reset-password" className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400">نسيت كلمة المرور؟</Link>
+              </div>
+              <div>
+                <Button className="w-full" size="sm">تسجيل الدخول</Button>
+              </div>
+            </div>
+          </form>
+          <Image src="/images/logo/logo-blue.png" alt="logo" width={350} className="max-sm:hidden" height={364} />
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
