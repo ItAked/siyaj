2ec773a6d73584a6dc63eb2dcd78def335e49452
xilujain <lujainboo@gmail.com>
2025-08-17 13:16:52 +0300

    change auth token

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl
     if (pathname.startsWith('/lawyer') || pathname.startsWith('/practitioner')) {
-        const token = request.cookies.get('auh_token')
+        const token = request.cookies.get('auhToken')
         if (!token) {
             return NextResponse.redirect(new URL('/', request.url));
         }
