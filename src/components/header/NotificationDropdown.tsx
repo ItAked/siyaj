50fb776617866f7523d2216e9678ab67dda80654
xilujain <lujainboo@gmail.com>
2025-08-20 09:32:28 +0300

    notification link

--- src/components/header/NotificationDropdown.tsx
+++ src/components/header/NotificationDropdown.tsx
@@ -93,8 +93,10 @@ export default function NotificationDropdown() {
             </li>
           ))}
         </ul>
-        <Link href="/" className="block px-4 py-2 mt-3 text-sm font-medium text-center text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-100
-        dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700">View All Notifications</Link>
+        {/* <Link href="/" className="block px-4 py-2 mt-3 text-sm font-medium text-center text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-100
+        dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700">عرض جميع التنبيهات</Link> */}
+        <a href="" className="block px-4 py-2 mt-3 text-sm font-medium text-center text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100
+        dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 link">عرض جميع التنبيهات</a>
       </Dropdown>
     </div>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "./utils/auth";
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl
     if (pathname.startsWith('/lawyer') || pathname.startsWith('/practitioner')) {
-        const token = getToken()
+        const token = request.cookies.get('authToken')
         if (!token) {
             return NextResponse.redirect(new URL('/', request.url));
         }
