6d9cafb5b208a3d11200be728c0bc059a2eb2456
xilujain <lujainboo@gmail.com>
2025-08-21 15:33:33 +0300

    feat: add Notifications component and create NotificationPage for displaying notifications

---
+++ src/components/Notifications.tsx
@@ -0,0 +1,12 @@
+import PageBreadcrumb from "./common/PageBreadCrumb"
+import NotificationCard from "./NotificationsCard"
+
+const Notifications = () => {
+    return (
+        <>
+            <PageBreadcrumb pageTitle="الإشعارات" />
+            <NotificationCard />
+        </>
+    )
+}
+export default Notifications
